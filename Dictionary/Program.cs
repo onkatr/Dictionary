97ce3dc5d595c66ee1520cc4fbbcd54efb91c347
onkatr <77552234+onkatr@users.noreply.github.com>
2021-01-25 11:11:39 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Dictionary
+{
+    class Dictionary
+{
+    static public void Main()
+    {
+        Dictionary<int, string> My_dictionary1 = new Dictionary<int, string>();
+
+        My_dictionary1.Add(1, "Onur");
+        My_dictionary1.Add(2, "Merve");
+        My_dictionary1.Add(3, "Derin");
+
+        foreach (KeyValuePair<int, string> item1 in My_dictionary1)
+        {
+            Console.WriteLine("{0} and {1}", item1.Key, item1.Value);
+        }
+    }
+}
+}
